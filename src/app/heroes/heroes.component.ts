deb4fb3fafa0e3434a7601eebecb822f5ffbadf4
TripleNine666 <kolumb1709@gmail.com>
2023-04-01 18:23:42 +0300

    add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,19 +8,9 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  constructor(
-    private heroServece: HeroService,
-    private messageService: MessageService
-  ) {}
-  selectedHero?: Hero;
+  constructor(private heroServece: HeroService) {}
   heroes: Hero[] = [];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(
-      `HeroesComponent: Selected Hero id:${this.selectedHero.id}`
-    );
-  }
   getHeroes(): void {
     this.heroServece.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
