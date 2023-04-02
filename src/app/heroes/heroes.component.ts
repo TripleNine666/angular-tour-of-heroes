9fd99a9c625db6e4b79f463b608d457f0af6185a
TripleNine666 <kolumb1709@gmail.com>
2023-04-02 13:27:09 +0300

    Get data from a server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,13 +8,30 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  constructor(private heroServece: HeroService) {}
+  constructor(private heroService: HeroService) {}
   heroes: Hero[] = [];
 
   getHeroes(): void {
-    this.heroServece.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
+
   ngOnInit() {
     this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
