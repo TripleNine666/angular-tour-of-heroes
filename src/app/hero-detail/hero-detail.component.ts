5569d22f260c35af24b905bb6783919d025b3f35
TripleNine666 <kolumb1709@gmail.com>
2023-04-24 12:24:11 +0300

    heroForm validation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,6 +11,7 @@ import {
   ValidationErrors,
   ValidatorFn,
   Validators,
+  FormBuilder,
 } from '@angular/forms';
 
 import { HeroService } from '../hero.service';
@@ -26,37 +27,70 @@ export class HeroDetailComponent {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
+  // Class, race, sex
   staticData = constants;
 
   existisHero: boolean = true;
+  // text in submit button
+  buttonCupture = 'Save';
 
-  hero: Hero = {
-    id: 0,
-    name: '',
-    age: 0,
-    class: '',
-    race: '',
-    sex: '',
-  };
-
-  heroForm = new FormGroup({
-    id: new FormControl(this.hero.id),
-    name: new FormControl(this.hero.name),
-    age: new FormControl(this.hero.age),
-    class: new FormControl(this.hero.class),
-    race: new FormControl(this.hero.race),
-    sex: new FormControl(this.hero.sex),
+  // FORM
+  heroForm = this.fb.group({
+    id: [0],
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я-_\.\ ]{0,20}$/),
+      ],
+    ],
+    age: [
+      0,
+      [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(1000),
+        Validators.pattern(/^[1-9]\d*$/),
+      ],
+    ],
+    class: ['', Validators.required],
+    race: ['', Validators.required],
+    sex: ['', Validators.required],
   });
 
+  get id() {
+    return this.heroForm.get('id');
+  }
+  get name() {
+    return this.heroForm.get('name');
+  }
+  get age() {
+    return this.heroForm.get('age');
+  }
+  get class() {
+    return this.heroForm.get('class');
+  }
+  get race() {
+    return this.heroForm.get('race');
+  }
+  get sex() {
+    return this.heroForm.get('sex');
+  }
+
   ngOnInit(): void {
+    //add hero
     if (this.router.url === '/add-hero') {
       this.existisHero = false;
-
-      this.heroService.getNewId().subscribe((id) => (this.hero.id = id));
+      // change text in button
+      this.buttonCupture = 'Add hero';
+      // generate id for new hero
+      this.heroService.getNewId().subscribe((id) => this.id?.setValue(id));
     } else {
+      // update hero
       this.getHero();
     }
   }
@@ -64,20 +98,33 @@ export class HeroDetailComponent {
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe((hero) => {
-      this.hero = hero;
       this.heroForm.patchValue(hero);
     });
   }
   goBack(): void {
     this.location.back();
   }
-  save(): void {
-    if (this.hero) {
-      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  clearName() {
+    this.heroForm.get('name')?.setValue('');
+  }
+  clearAge() {
+    this.heroForm.get('age')?.setValue(null);
+  }
+  onSubmit(): void {
+    // if valid
+    if (this.heroForm.invalid) {
+      return;
+    }
+    // update
+    if (this.existisHero) {
+      this.heroService
+        .updateHero(this.heroForm.value as Hero)
+        .subscribe(() => this.goBack());
+    } else {
+      // add
+      this.heroService
+        .addHero(this.heroForm.value as Hero)
+        .subscribe(() => this.goBack());
     }
   }
-  add(): void {
-    this.heroService.addHero(this.hero).subscribe();
-  }
-  onSubmit() {}
 }
